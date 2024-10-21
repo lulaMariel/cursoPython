d2fb7e251aefd91c901a35219e575969d872130c
lulaMariel <luciana.lavalle@gmail.com>
2024-10-21 02:07:52 -0300

    fin clase 3

---
+++ Clase 3/tiposDeDatos.py
@@ -0,0 +1,35 @@
+x = 5 # Entero
+numero = 9.3 # Float
+nombre = "Paula"
+apellido = "Pérez"
+
+print("\n")
+print(x)
+print(numero)
+print(nombre)
+print(apellido)
+print("\n")
+
+type(x)
+type(numero)
+type(nombre)
+type(apellido)
+print("\n")
+
+print(type(x))
+print(type(numero))
+print(type(nombre))
+print(type(apellido))
+print("\n")
+
+# Función bool(): Indica si un valor es True (valor almacenado) o False(nulo o vacío).
+num = 0
+nom = ""
+
+print("\n")
+print(bool(num)) # False
+print(bool(nom)) # False
+print(bool(x)) # True
+print(bool(numero)) # True
+print(bool(nombre)) # True
+print(bool(apellido)) # True
